038cdab3afc6c529c09120a55938cbafb283d66b
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2023-02-16 12:53:11 +0100

    TEST simple python examples for IRB6700 rrc communication

--- robot_files/abb_irb_6700_track_irtb_6004/5_playgrounds/python/get_and_move_to_target.py
+++ robot_files/abb_irb_6700_track_irtb_6004/5_playgrounds/python/get_and_move_to_target.py
@@ -19,29 +19,20 @@ if __name__ == '__main__':
     # Set work object
     abb.send(rrc.SetWorkObject('wobj0'))
 
-    # Read current frame position
-    #frame = abb.send_and_wait(rrc.GetFrame())
-
-    # Change the x-value [mm]
-    #frame.point[0] -= 50
-
-    # Set speed [mm/s]
-    #speed = 100
-    
-    # Move robot the new pos
-    #done = abb.send_and_wait(rrc.MoveToFrame(frame, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
-
-    # Move robot the new pos
-    #done = abb.send_and_wait(rrc.MoveToTarget(frame, speed, rrc.Zone.FINE, rrc.Motion.LINEAR))
-
     # Get frame and external axes 
     frame, external_axes = abb.send_and_wait(rrc.GetRobtarget())
 
     # Print received values
     print(frame, external_axes)
 
-    # Print received values
-    print(frame)
+    # Change the x-value [mm]
+    frame.point[0] -= 50
+
+    # Set speed [mm/s]
+    speed = 100
+
+    # Move robot the new pos
+    done = abb.send_and_wait(rrc.MoveToRobtarget(frame, external_axes, speed, rrc.Zone.FINE))
 
     # Print feedback 
     print('Feedback = ', done)
