6f5c9ee78d17dba4b07dde8b196500310fa490be
alexpittig <alexandra.pittiglio@gmail.com>
2024-06-28 15:58:04 +0200

    Update calibration_rpy.py

--- robot_files/abb_irb_6700_track_irtb_6004_iris/5_playgrounds/python/calibration_rpy.py
+++ robot_files/abb_irb_6700_track_irtb_6004_iris/5_playgrounds/python/calibration_rpy.py
@@ -7,11 +7,11 @@
 import numpy as np
 from compas.geometry import Rotation, Transformation, Translation, Vector, Frame, Point, Quaternion
 
-w = 1
-x = 0.000334521
-y = 0.000334521
-z = 0.0000178281
-Q = Quaternion(w, x, y,z)
+q1 = 1
+q2 = 0.000334521
+q3 = 0.000334521
+q4 = 0.0000178281
+Q = Quaternion(q1, q2, q3, q4)
 R = Rotation.from_quaternion(Q)
 
 rpy = R.euler_angles(static=True, axes ='xyz')
